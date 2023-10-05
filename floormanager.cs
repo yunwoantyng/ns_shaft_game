cc71cda41c91f5509c6a13259bd65b58e91e6c49
Wan-Ting Yun <101507334+yunwoantyng@users.noreply.github.com>
2023-10-05 00:32:12 -0400

    Create floormanager.cs

---
+++ floormanager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorManager : MonoBehaviour
+{
+    [SerializeField] GameObject[] floorPrefabs;
+    public void SpawnFloor()
+    {
+        int r = Random.Range(0, floorPrefabs.Length);
+        GameObject floor = Instantiate(floorPrefabs[r], transform);
+        floor.transform.position = new Vector3(Random.Range(-3.8f, 3.8f), -6f, 0f);
+    }
+}
